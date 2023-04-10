cc7877db7828f2a9571afad75cc5259d6dd5f610
Danil <mydogfromhell666@gmail.com>
2023-04-10 17:14:47 +0300

    add: config decomposition

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+import path from "path";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+
+
+export function buildWebpackConfig (options: BuildOptions): webpack.Configuration {
+    const {paths, mode} = options
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+    }
+}
